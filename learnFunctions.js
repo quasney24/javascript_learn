ad58f042896ec1a726f29a8b6f0c22d7efc440fa
Zachary Quasney <zacharyquasney@gmail.com>
2018-09-05 16:17:54 -0400

    functions

---
+++ learnFunctions.js
@@ -0,0 +1,23 @@
+for(var i=-10; i<20; i++){
+	console.log(i);
+	}
+
+for(var i=10; i<41; i+=2){
+	console.log(i);
+}
+for(var i=10; i<=40; i+=1){
+	if(i % 2 === 0){
+	console.log(i);
+	}
+}
+
+for(var i=300; i<=333; i++){
+	if(i % 2 !== 0){
+	console.log(i);
+	}
+}
+function dosomething(){
+	console.log("hello world");
+}
+
+dosomething()
